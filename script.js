61b4c758dbd19b7145f012817ffc0421cfc03b2c
Your NameJabanes <erezhabani2003@gmail.com>
2024-12-08 13:25:06 +0200

    Added Winner, reset score, and round system

--- script.js
+++ script.js
@@ -5,80 +5,112 @@ ar = ["rock", "paper", "scissors"]
 
 let userScore = 0
 let botScore = 0
+round = 1
 
+function reset() {
+    userScore = 0
+    botScore = 0
+    document.getElementById("userscore").textContent = `You: `
+    document.getElementById("botscore").textContent = `Bot: `
+    round = 1
+    return
+}
 
 function submit() {
-    
-    function showResult() {
 
-        if (botPick != userPick) {
-    
-            if (botPick == "rock" && userPick == "scissors") {
-                
-                document.getElementById("userpick").textContent = `You Picked: ${userPick}`
-                document.getElementById("botpick").textContent = `Bot Picked: ${botPick}`
-                document.getElementById("Result").textContent = `You Lose!`
-                console.log("You lose!");
-                return botScore += 1
-    
-            }
-            else if (botPick == "paper" && userPick == "rock") {
-                document.getElementById("userpick").textContent = `You Picked: ${userPick}`
-                document.getElementById("botpick").textContent = `Bot Picked: ${botPick}`
-                document.getElementById("Result").textContent = `You Lose!`
-                console.log("You lose!");
-                return botScore += 1
+    document.getElementById("Round").textContent = `Round: ${round}`
+    round += 1
+    console.log(`round: ${round}`);
+    while (round <= 10) {
 
-                
-            }
-            else if (botPick == "scissors" && userPick == "paper") {
-                document.getElementById("userpick").textContent = `You Picked: ${userPick}`
-                document.getElementById("botpick").textContent = `Bot Picked: ${botPick}`
-                document.getElementById("Result").textContent = `You Lose!`
-                console.log("You lose!");
-                return botScore += 1
+        function showResult() {
+
+            if (botPick != userPick) {
+
+                if (botPick == "rock" && userPick == "scissors") {
+
+                    document.getElementById("userpick").textContent = `You Picked: ${userPick}`
+                    document.getElementById("botpick").textContent = `Bot Picked: ${botPick}`
+                    document.getElementById("Result").textContent = `You Lose!`
+                    console.log("You lose!");
+                    return botScore += 1
+
+                }
+                else if (botPick == "paper" && userPick == "rock") {
+                    document.getElementById("userpick").textContent = `You Picked: ${userPick}`
+                    document.getElementById("botpick").textContent = `Bot Picked: ${botPick}`
+                    document.getElementById("Result").textContent = `You Lose!`
+                    console.log("You lose!");
+                    return botScore += 1
+
+
+                }
+                else if (botPick == "scissors" && userPick == "paper") {
+                    document.getElementById("userpick").textContent = `You Picked: ${userPick}`
+                    document.getElementById("botpick").textContent = `Bot Picked: ${botPick}`
+                    document.getElementById("Result").textContent = `You Lose!`
+                    console.log("You lose!");
+                    return botScore += 1
+
+                }
+                else {
+                    document.getElementById("userpick").textContent = `You Picked: ${userPick}`
+                    document.getElementById("botpick").textContent = `Bot Picked: ${botPick}`
+                    document.getElementById("Result").textContent = `You Win!`
+                    console.log("You win!");
+                    userScore += 1
+
+
+                }
 
             }
             else {
                 document.getElementById("userpick").textContent = `You Picked: ${userPick}`
                 document.getElementById("botpick").textContent = `Bot Picked: ${botPick}`
-                document.getElementById("Result").textContent = `You Win!`
-                console.log("You win!");
-                userScore += 1
-               
-    
+                document.getElementById("Result").textContent = `Thats a tie!`
+                console.log("Thats a tie!");
+
             }
-    
+
         }
+
+        document.getElementById("winner").textContent = ``;
+        let userPick = document.getElementById("Pick").value;
+        let botPick = ar[Math.floor(Math.random() * ar.length)];
+        console.log(`userPick: ${userPick}`);
+        console.log(`botPick: ${botPick}`);
+
+        if (ar.includes(userPick)) {
+
+            showResult()
+        }
+
         else {
-            document.getElementById("userpick").textContent = `You Picked: ${userPick}`
-            document.getElementById("botpick").textContent = `Bot Picked: ${botPick}`
-            document.getElementById("Result").textContent = `Thats a tie!`
-            console.log("Thats a tie!");
-    
+            console.log("Invalid Choice");
         }
-    
+
+        document.getElementById("userscore").textContent = `You: ${userScore}`
+        document.getElementById("botscore").textContent = `Bot: ${botScore}`
+
+        return
+
+
     }
-    
-
-    let userPick = document.getElementById("Pick").value;
-    let botPick = ar[Math.floor(Math.random() * ar.length)];
-    console.log(`userPick: ${userPick}`);
-    console.log(`botPick: ${botPick}`);
-
-    if (ar.includes(userPick)) {
-
-        showResult()
+    if (userScore > botScore) {
+        document.getElementById("winner").textContent = `CONGRATULATIONS!, YOU WON THE GAME!`;
+        reset()
+        return
     }
-
     else {
-        console.log("Invalid Choice");
+        document.getElementById("winner").textContent = `sorry, you lost the game...`;
+        reset()
+        return
     }
 
-    document.getElementById("userscore").textContent = `You: ${userScore}`
-    document.getElementById("botscore").textContent = `Bot: ${botScore}`
-    return 
+
 }
+
+
 
 
 
